a171c7a31263b2363553300ccecdd4f419fee35c
stepan <stepan-alekseev-87@mail.ru>
2022-12-16 10:24:00 +0600

    task 3: added tests with @BeforeEach, @BeforeAll.

---
+++ src/test/java/FourthTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.*;
+
+/**
+ * Задание 3.4.c : методы @BeforeEach выводятся в том порядке, в котором
+ * я их определил.
+ * Задание 3.4.e поставил определение before1 после before3. В итоге текст
+ * вывелся в таком же порядке, как в 3.4.c до перемещения... Видимо, их порядок
+ * устанавливается изначально и дальше может меняться (а может и не меняться)
+ * на усмотрение JVM.
+ * Задание 3.4.g впечатление, что JVM решает запускать тесты, сначала
+ * отсортировав их по названию в лексикографическом порядке. На самом деле судя
+ * по документации там по-другому сделано: https://github.com/junit-team/junit5/issues/2990#issuecomment-1207248587
+ */
+public class FourthTest {
+
+    // @BeforeEach
+    void beforeD() {
+        System.out.println("выполнения ");
+    }
+
+    @BeforeEach
+    void beforeE() {
+        beforeD();
+        System.out.println("нескольких ");
+    }
+
+    @BeforeAll
+    static void beforeB() {
+        System.out.println("Порядок");
+    }
+
+    @AfterEach
+    void beforeA() {
+        System.out.println("Before ");
+        beforeF();
+    }
+
+    //@BeforeEach
+    void beforeF() {
+        System.out.println("методов ");
+    }
+
+    @AfterAll
+    static void beforeC() {
+        System.out.println("недетерминирован");
+    }
+
+    @Test
+    void test10() {
+        System.out.print("");
+    }
+}
---
+++ src/test/java/SecondTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.*;
+
+/**
+ * На примере данного класса можно сделать вывод о вызовах методов:
+ * @BeforeAll родителя
+ * @BeforeAll потомка
+ *
+ * @BeforeEach родителя
+ * @BeforeEach потомка
+ * @Test
+ * @AfterEach потомка
+ * @AfterEach родителя
+ *
+ * @AfterAll потомка
+ * @AfterAll родителя
+ */
+public class SecondTest extends FirstTest {
+    @BeforeAll
+    static void beforeAllSecond() {
+        System.out.println("SecondTest start");
+    }
+
+    @BeforeEach
+    void beforeEachSecond() {
+        System.out.println("Тест запущен");
+    }
+
+    @AfterEach
+    void afterEachSecond() {
+        System.out.println("Тест остановлен");
+    }
+
+
+    @AfterAll
+    static void afterAllSecond() {
+        System.out.println("SecondTest stop");
+    }
+
+    @Test
+    void test6() {
+        System.out.println("Test 6");
+    }
+
+    @Test
+    void test7() {
+        System.out.println("Test 7");
+    }
+}
