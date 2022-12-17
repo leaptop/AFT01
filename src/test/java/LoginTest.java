78d37960105e837300cf9e3c115567ed7522a283
stepan <stepan-alekseev-87@mail.ru>
2022-12-17 19:18:43 +0600

    task 6 work with a new branch task6-7. Searching elements by their attributes and xpath

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,37 +1,68 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class LoginTest {
+    public WebDriver chromedriver;
+
     /**
-     * В данном тесте нужно дождаться загрузки страницы и потом максимизировать её. За факт загрузки страницы
-     * я решил принять видимость кнопки с надписью "Войти". Понимаю, что использование искспаса в тесте не допустимо
-     * , но здесь руководствуюсь правилом KISS.
+     * Инициализируем вебдрайвер, настраиваем его перед запуском каждого теста.
      */
-    @Test
-    void initWebDriver() {
+    @BeforeEach
+    void initTests() {
         System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
         ChromeOptions options = new ChromeOptions();
-        Dimension newDimension = new Dimension(200, 100);
-
-        options.addArguments("--window-size=500,500");
-        WebDriver chromedriver = new ChromeDriver(options);
-        chromedriver.get("https://tt-develop.quality-lab.ru");
-
-        chromedriver.manage().window().setSize(newDimension);
-        WebDriverWait wait = new WebDriverWait(chromedriver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@value=\"Войти\"]")));
-
+        //options.addArguments("--window-size=500,500");
+        chromedriver = new ChromeDriver(options);
         chromedriver.manage().window().maximize();
+    }
+
+    /**
+     * Метод завершает работу вебдрайвера после выполнения каждого теста.
+     */
+    @AfterEach
+    void finishTest() throws InterruptedException {
+        Thread.sleep(3000);
         chromedriver.quit();
+    }
 
-
+    /**
+     * https://tt-develop.quality-lab.ru это вроде не подойдёт
+     * https://tt-testing.quality-lab.ru/login а это вроде подойдёт
+     * Ввести в поле “Имя пользователя” значение “TestUser”. Искать поле по атрибуту name
+     * Ввести в поле “Пароль” значение “Password”. Искать поле по атрибуту id
+     * Проверить, что на странице нет текста “Invalid credentials.”.
+     * Он появится после нажатия кнопки (Войти). Искать блок при помощи xpath (ищем блок содержащий текст)
+     * Нажать кнопку “Войти”. Искать кнопку по атрибуту value
+     * Проверить что сайт выдал текст “Invalid credentials.”.
+     * Запустить тест
+     * Выложить изменения в репозиторий
+     */
+    @Test
+    void incorrectUserNameAndPassword() {
+        String xpathForInvalidCredentialsText = "//div[contains(text(), 'Invalid credentials.')]";
+        chromedriver.get("https://tt-testing.quality-lab.ru/login");
+        chromedriver.findElement(By.name("_username")).sendKeys("TestUser");
+        chromedriver.findElement((By.id("password"))).sendKeys("Password");
+        Assertions.assertTrue(chromedriver.findElements(By.xpath(
+                        "//div[contains(text(), 'Invalid credentials.')]")).size() == 0
+                , "Надпись найдена");
+        chromedriver.findElement(By.xpath("//*[@value='Войти']")).click();
+        Assertions.assertTrue(chromedriver.findElements(By.xpath(
+                        "//div[contains(text(), 'Invalid credentials.')]")).size() > 0
+                , "Надпись не найдена"
+        );
     }
 }
