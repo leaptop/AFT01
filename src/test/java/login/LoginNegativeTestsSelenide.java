0f9ec5498c5fee85b0be4cebe578ae85d12c60d2
stepan <stepan-alekseev-87@mail.ru>
2022-12-21 23:29:31 +0600

    Реализовал тест на чистом Selenide с проверкой реакции на нажатие кнопки без ввода логина и пароля

--- src/test/java/login/LoginNegativeTestsSelenide.java
+++ src/test/java/login/LoginNegativeTestsSelenide.java
@@ -1,38 +1,14 @@
 package login;
 
-import autotests.TestBase;
-import org.junit.jupiter.api.Assertions;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import pages.LoginPageSelenide;
-import ru.yandex.qatools.htmlelements.element.TextInput;
-
-import javax.xml.datatype.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverConditions.url;
 
-/**
- * Переписать существующие тесты используя библиотеки Selenide и HTMLElements а именно:
- * Открыть браузер при помощи Selenide
- * Задать начальный размер экрана браузера при помощи Selenide
- * Изменять размеры окна браузера через Selenium, но получить текущий браузер от Selenide через WebDriverRunner
- * Переписать все локаторы в PageObject: использовать HTMLElements для указания типа контрола, Selenide - для поиска
- * При необходимости переписать методы работы с контролами в PageObject. (Например у Selenide есть очень удобный метод exists())
- * Переписать методы PageObject чтобы можно было использовать цепочки вызовов (Method chaining)
- * Подумать какая комбинация этих (или возможно других) библиотек наиболее удобна для написания автотестов и почему? (нужно выработать свое мнение, а не искать чужое в интернете).
- * Запустить все тесты
- * Выложить изменения в репозиторий
- * Самопроверка задания
- * Все тесты зеленые
- * Локаторы имеют вид:
- * private TextInput login = new TextInput($("#username"));
- * Все методы действия в PageObject (т.е  методы ввода логина-пароля и нажатия на кнопку) возвращают this
- */
-
-public class LoginNegativeTestsSelenide extends TestBase {
-    //private TextInput login = new TextInput($("#username"));
-
+public class LoginNegativeTestsSelenide {
     /**
      * Проверка того, что текст о неверных данных (о логине и пароле) не выведется при нажатии на кнопку "Войти"
      * с пустыми полями логина и пароля.
@@ -40,20 +16,14 @@ public class LoginNegativeTestsSelenide extends TestBase {
      * Проверка того, что не произойдёт перенаправления на какие-либо другие страницы при вышеописанных действиях.
      */
     @Test
-    public void checkEmptyLoginPassword() {
-        open("https://tt-testing.quality-lab.ru/login", LoginPageSelenide.class)
-                .clickEnterButton();
-//        Assertions.assertFalse(lps.invalidCredentialsTextIsVisible(),
-//                "Исключение не было выброшено, т.к. элемент был найден");
-//        webdriver().shouldHave(url("https://tt-testing.quality-lab.ru/login"));
+    public void checkEmptyLoginPassword() throws InterruptedException {
+        Configuration.browserSize = "1000x1000";
+        open("https://tt-testing.quality-lab.ru/login"
+                , LoginPageSelenide.class).clickEnterButton();
+        $x("//div[contains(text(), 'Invalid credentials.')]").shouldNotBe(Condition.visible);
+        webdriver().shouldHave(url("https://tt-testing.quality-lab.ru/login"));
 
-//        open("https://tt-testing.quality-lab.ru/login", LoginPageSelenide.class);
-//        LoginPageSelenide lps = new LoginPageSelenide(chromedriver);
-//        lps.clickEnterButton();
-//        Assertions.assertFalse(lps.invalidCredentialsTextIsVisible(),
-//                "Исключение не было выброшено, т.к. элемент был найден");
-//        webdriver().shouldHave(url("https://tt-testing.quality-lab.ru/login"));
+
+        Thread.sleep(4000);
     }
-
-
 }
--- src/test/java/pages/LoginPageSelenide.java
+++ src/test/java/pages/LoginPageSelenide.java
@@ -2,7 +2,9 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import ru.yandex.qatools.htmlelements.element.TextInput;
 
+import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
@@ -24,7 +26,14 @@ public class LoginPageSelenide {
         $(By.xpath(xpathForEnterButton)).click();
         return this;
     }
-
+//    private TextInput login; //= new TextInput($("#username"));//By.name("_username")
+//    private TextInput login2 ;
+//    public LoginPageSelenide insertText(String textToSend){
+//        //login2 = new TextInput($(byAttribute("name", "_username")));
+////        login = new TextInput($("#username"));//By.name("_username")
+////        login.click();
+////        return this;
+//    }
     public boolean invalidCredentialsTextIsVisible() {
         return $x(xpathForInvalidCredentialsText).exists();
     }
