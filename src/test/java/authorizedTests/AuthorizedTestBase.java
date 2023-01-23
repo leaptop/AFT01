5ee04b2c476ed28502a69ac16e05bbe02652d8aa
stepan <stepan-alekseev-87@mail.ru>
2023-01-23 20:57:29 +0600

    Начал 22 задание. Сохранил пейджу, подготовил всё к работе с сайтом ЛК.

---
+++ src/test/java/authorizedTests/AuthorizedTestBase.java
@@ -0,0 +1,77 @@
+package authorizedTests;
+
+import com.codeborne.selenide.WebDriverRunner;
+import helpers.TestBase;
+import okhttp3.*;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selenide.open;
+//import static properties.Properties.credentialsProperties;
+
+/**
+ * @author Алексеев Степан
+ * @date 25.12.2022
+ */
+public class AuthorizedTestBase extends TestBase {
+    /**
+     * Авторизация по АПИ.
+     */
+    @BeforeMethod
+    public void authorizeViaHTTP() {
+        CookieManager cookieManager = new CookieManager();//1 start
+        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+        JavaNetCookieJar cookieJar = new JavaNetCookieJar(cookieManager);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .cookieJar(cookieJar)
+                .followRedirects(false)
+                .build();//1 end
+        RequestBody formBody = null;
+        try {
+            formBody
+                    = new FormBody.Builder(StandardCharsets.UTF_8)//2
+                    .add("_csrf_token", "")
+                    .add("_username", "Авто Пользователь")
+                    .add("_password", "12345678")
+                    .add("_submit", "Войти")
+                    .build();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Request getRequest = new Request.Builder()//это нужно чтобы получить заголовки, без которых post не сработает
+                .url("https://tt.quality-lab.ru/login")
+                .addHeader("Connection", "keep-alive")
+                .build();
+        Request postRequest = new Request.Builder()
+                .url("https://tt.quality-lab.ru/login_check")
+                .addHeader("Connection", "keep-alive")
+                .post(formBody)
+                .build();
+        try {
+            Response response1 = client.newCall(getRequest).execute();
+            Response response2 = client.newCall(postRequest).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }//2 end
+        open("https://tt.quality-lab.ru");
+        WebDriverRunner.clearBrowserCache();
+
+        cookieManager.getCookieStore().getCookies().forEach(httpCookie -> {//3c
+            org.openqa.selenium.Cookie cookie = new org.openqa.selenium
+                    .Cookie(
+                    httpCookie.getName(),
+                    httpCookie.getValue(),
+                    httpCookie.getDomain(),
+                    httpCookie.getPath(),
+                    null
+            );
+            WebDriverRunner.driver().getWebDriver().manage().addCookie(cookie);
+        });
+    }
+}
---
+++ src/test/java/authorizedTests/calendar/CalendarTest.java
@@ -0,0 +1,146 @@
+package authorizedTests.calendar;
+
+import authorizedTests.AuthorizedTestBase;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.calendar.CalendarPO;
+import pages.calendar.Day;
+import pages.calendar.Snippet;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.util.ArrayList;
+
+import static com.codeborne.selenide.Selenide.open;
+
+/**
+ * @author Алексеев Степан
+ * @date 25.12.2022
+ */
+public class CalendarTest extends AuthorizedTestBase {
+    public CalendarPO calendarPO;
+    @BeforeClass
+    static void setupAllureReports() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(false)
+        );
+    }
+    /**
+     * Проверка существования выходных и рабочих дней в календаре
+     */
+    @Step("Проверка существования выходных и рабочих дней в календаре")
+    private void holidaysAndWorkDaysExistenceCheckNew() {
+        boolean foundHoliday = false;
+        boolean foundWorkDay = false;
+        ArrayList<LocalDate> datesToCheck = calendarPO.getDates();
+        for (int i = 0; i < datesToCheck.size(); i++) {
+            calendarPO.clickDayOfThisMonth(datesToCheck.get(i));
+            Day day = calendarPO.getDay(datesToCheck.get(i));
+            if (day.isHoliday()) {
+                foundHoliday = true;
+            } else if (day.isWorkDay()) {
+                foundWorkDay = true;
+            }
+            if (foundHoliday && foundWorkDay) {
+                break;
+            }
+        }
+        boolean finalFoundWorkDay = foundWorkDay;
+        boolean finalFoundHoliday = foundHoliday;
+        SoftAssert soft = new SoftAssert();
+        soft.assertTrue(finalFoundWorkDay, "Не найдены рабочие дни в месяце");
+        soft.assertTrue(finalFoundHoliday, "Не найдены выходные дни в месяце");
+        soft.assertAll();
+    }
+
+    /**
+     * Открываем календарь и ждём появления и исчезновения Progress bar.
+     */
+    @BeforeMethod
+    @Step("Открываем календарь и ждём появления и исчезновения Progress bar-а")
+    public void openCalendar() {
+        calendarPO = open("https://tt.quality-lab.ru/calendar/", CalendarPO.class)
+                .waitForCalendarToLoad();
+    }
+
+    /**
+     * 1 Сценарий: проверка текущего месяца
+     * Проверить:
+     * месяц и год совпадают с текущими
+     * в месяце есть рабочие дни (зеленые)
+     * в месяце есть выходные (визуально пустые, но внутри есть плашка аналогично рабочим дням, только белая)
+     */
+    @Test
+    public void checkCurrentMonthAndYear() {
+        SoftAssert soft = new SoftAssert();
+        soft.assertEquals(LocalDateTime.now().getMonth(), calendarPO.getMonth(),
+                "Текущий месяц не совпадает с выведенным на календаре");
+        soft.assertEquals(Year.of(LocalDateTime.now().getYear()).getValue(), calendarPO.getYear().getValue(),
+                "Текущий год не совпадает с выведенным на календаре");
+        soft.assertAll();
+        holidaysAndWorkDaysExistenceCheckNew();
+    }
+
+    /**
+     * 2 Сценарий: проверка переключения месяца
+     * Выбрать следующий месяц и нажать кнопку “Применить”
+     * Проверить:
+     * в месяце есть рабочие дни (зеленые)
+     * в месяце есть выходные (визуально пустые, но внутри есть плашка аналогично рабочим дням, только белая)
+     */
+    @Test
+    public void checkMonthSwitch() {
+        calendarPO.chooseNextMonth();
+        holidaysAndWorkDaysExistenceCheckNew();
+    }
+
+    /**
+     * 3 Сценарий: проверка графика другого сотрудника:
+     * Выбрать любого другого сотрудника (в тест-методе использовать фиксированную фамилию) и нажать кнопку “Применить”
+     * Проверить:
+     * в месяце есть рабочие дни (зеленые)
+     * в месяце есть выходные (визуально пустые, но внутри есть плашка аналогично рабочим дням, только белая)
+     */
+    @Test
+    public void checkOtherEmployee() {
+        calendarPO.chooseEmployee("Якина");
+        holidaysAndWorkDaysExistenceCheckNew();
+    }
+
+    /**
+     * 4 Сценарий: проверка переключения бокового сниппета
+     * Реализуй при помощи неявных ожиданий
+     * Для каждого дня в календаре выполнить:
+     * Клик по дню в календаре
+     * Проверить что информация в боковом снипете совпадает с информацией в дне
+     */
+    @Test
+    public void checkSideSnippetSwitchNew() {
+        ArrayList<LocalDate> datesToCheck = calendarPO.getDates();
+        for (int i = 0; i < datesToCheck.size(); i++) {
+            calendarPO.clickDayOfThisMonth(datesToCheck.get(i));
+            Day day = calendarPO.getDay(datesToCheck.get(i));
+            Snippet snippet = calendarPO.getSnippet();
+            if (day.isHoliday()) {
+                SoftAssert soft = new SoftAssert();
+                soft.assertEquals("", snippet.getEvents().get(0));
+                soft.assertEquals("Выходной", snippet.getEvents().get(1));
+                soft.assertAll();
+            } else if (day.isWorkDay() || day.isVacationDay()) {
+                ArrayList<String> dayEvents = day.getEvents();
+                ArrayList<String> snippetEvents = snippet.getEvents();
+                for (int j = 0; j < dayEvents.size(); j++) {
+                    Assert.assertEquals(dayEvents.get(j), snippetEvents.get(j * 2));
+                }
+            }
+        }
+    }
+}
