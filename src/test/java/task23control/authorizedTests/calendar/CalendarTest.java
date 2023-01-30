db752be4cb06765ab2220699447dcbddeb8433e2
stepan <stepan-alekseev-87@mail.ru>
2023-01-30 15:44:34 +0600

    Реализовал первые два шага задания 23 (контрольного). Унаследовал CalendarPO от SuccessfulLoginPage для доступа к меню слева. Потратил 1 час.

---
+++ src/test/java/task23control/authorizedTests/calendar/CalendarTest.java
@@ -0,0 +1,37 @@
+package task23control.authorizedTests.calendar;
+
+import org.testng.annotations.Test;
+import task23control.authorizedTests.AuthorizedTestBase;
+import task23control.pages.calendar.CalendarPO;
+
+import static com.codeborne.selenide.Selenide.open;
+import static properties.Properties.credentialsProperties;
+
+/**
+ * Предусловие:  Авторизоваться под пользователем через АПИ
+ * Шаги:
+ * 1 Открыть страницу /calendar/
+ * 2 Перейти в раздел Отчеты за сегодня через боковое меню (слева Отчеты - Отчеты за сегодня).
+ * 3 Нажать на нужное настроение
+ * 4 Проверить наличие всплывающего окна с надписью: “Вы хотите залогировать больше или меньше 8 часов, которые по
+ * графику запланированы у вас на сегодня. “
+ * 5 Нажать Отмена во всплывающем окне
+ * <p>
+ * Автотест можешь сделать в том же проекте что и остальную песочку, но чтобы потренироваться советую создать отдельный.
+ * Автотест необходимо параметризовать: использовать двух пользователей  и три разных настроения для каждого пользователя (т.е. всего будет 6 тестов)
+ * Необходимо прикрутить аллюр-отчет и реализовать параллельный запуск тестов
+ * Стек: Java (выше 8), Selenide и TestNG/JUnit на твой выбор, но с условием: выбираешь тот тестовый фреймворк, который тебе дается сложнее или который хуже знаешь
+ *
+ * @author Алексеев Степан
+ * @date 30.01.2023
+ */
+public class CalendarTest extends AuthorizedTestBase {
+    @Test
+    void test() {
+        open(credentialsProperties.urlCalendar(), CalendarPO.class);
+        CalendarPO calendarPO = new CalendarPO();
+        calendarPO.clickSubmenuOfReportMenuNamed("Отчет за сегодня");
+
+
+    }
+}
---
+++ src/test/java/task23control/pages/SuccessfulLoginPage.java
@@ -0,0 +1,126 @@
+package task23control.pages;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+import ru.yandex.qatools.htmlelements.element.Button;
+import ru.yandex.qatools.htmlelements.element.Image;
+import ru.yandex.qatools.htmlelements.element.TextBlock;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+/**
+ * Класс, реализующий паттерн Page Object для страницы, показываемой после успешного входа в систему по логину и паролю.
+ */
+public class SuccessfulLoginPage {
+    /**
+     * xpath для поиска аватарки пользователя справа вверху.
+     */
+    private String xpathForUpperRightCornerAvatar = "//span[@class='m-topbar__userpic']//div[@class='avatarCover']";
+    /**
+     * xpath для поля логина
+     */
+    private String xpathForUserNameOnCard = "//span[contains(@class, 'm-card-user__name')]";
+    /**
+     * xpath для поля пароля
+     */
+    private String xpathForUserEmailOnCard = "//span[contains(@class, 'm-card-user__email')]";
+    /**
+     * Текстовое поле с имейлом пользователя на карточке после нажатия на аватарку справа сверху
+     */
+    private String xpathForRightMenuCalendarButton =
+            "//li[@class='m-menu__item m-menu__item--submenu']/a/i[@class='m-menu__link-icon flaticon-calendar-2']";
+    private String xpathForMainMenuButton = "//a[@id='m_aside_left_minimize_toggle']";
+    private String xpathForSubMenu = "//ul[contains(@class, 'm-menu__nav')]/li//span[text()='%s']";
+    private String xpathForMainMenuItems = "//ul[contains(@class, 'm-menu__nav')]/li[%d]";
+    /**
+     * Иконка кнопки меню с графиком. Открывает подменю с отчётами.
+     */
+    private String xpathForReportIconMenuItem =
+            "//span[@class='m-menu__item-here']/following-sibling::i[contains(@class, 'flaticon-line-graph')]";
+
+    private TextBlock emailBlock = new TextBlock($x(xpathForUserEmailOnCard));
+    /**
+     * Текстовое поле с именем и фамилией пользователя на карточке после нажатия на аватарку справа сверху
+     */
+    private TextBlock nameBlock = new TextBlock($x(xpathForUserNameOnCard));
+    /**
+     * Аватарка справа сверху
+     */
+    private Image upperRightCornereAvatar = new Image($x(xpathForUpperRightCornerAvatar));
+
+    private Button rightMenuCalendarButton = new Button($x(xpathForMainMenuButton));
+
+    /**
+     * Весь функционал меню слева нужно вынести в базовую пейджу
+     * Наводит мышь на картинку с графиком (Отчёты) и нажимает пункт подменю по имени
+     *
+     * @param submenuItem имя пункта подменю
+     * @return
+     */
+    public SuccessfulLoginPage clickSubmenuOfReportMenuNamed(String submenuItem) {
+        $x(xpathForReportIconMenuItem).hover();
+        $x(xpathForReportIconMenuItem).$x(String.format("./ancestor::li//span[contains(text(),'%s')]", submenuItem))
+                .hover().click();
+        return this;
+    }
+
+    /**
+     * Сначала надо вызвать hoverOnMenuItemNumber, чтобы навести на пункт меню, потом вызывать этот метод для выбора
+     * пункта подменю по названию.
+     *
+     * @param name
+     * @return
+     */
+    public SuccessfulLoginPage clickOnSubMenuItemNamed(String name) {
+        $x(String.format(xpathForSubMenu, name)).click();
+        return this;
+    }
+
+    /**
+     * Наводит мышь на пункт главного меню по его номеру, считая сверху вниз.
+     *
+     * @param number
+     * @return
+     */
+    public SuccessfulLoginPage hoverOnMenuItemNumber(int number) {
+        $x(String.format(xpathForMainMenuItems, number)).hover();
+        return this;
+    }
+
+    @Step("Кликаем на кнопку главного меню")
+    public SuccessfulLoginPage clickMainMenuButton() {
+        $x(xpathForMainMenuButton)
+                .shouldBe(Condition.visible)
+                .click();
+        return this;
+    }
+
+    /**
+     * @return возвращает текстовый блок с имейлом пользователя с карточки после нажатия на аватарку справа сверху
+     */
+    @Step("Получаем объект с текстом имейла")
+    public TextBlock getEmailTextBlock() {
+        $x(xpathForUserEmailOnCard).shouldBe(Condition.visible);
+        return emailBlock;
+    }
+
+    /**
+     * @return Возвращает текстовый блок имени и фамилии с карточки после нажатия на аватарку справа сверху
+     */
+    @Step("Получаем объект с текстом имени")
+    public TextBlock getNameTextBlock() {
+        $x(xpathForUserNameOnCard).shouldBe(Condition.visible);
+        return nameBlock;
+    }
+
+    /**
+     * Кликает по аватарке в правом верхнем углу экрана.
+     *
+     * @return возвращает текущую страницу для возможности запуска других методов по цепочке.
+     */
+    @Step("Кликаем по аватарке справа сверху")
+    public SuccessfulLoginPage clickUpperRightCornerAvatar() {
+        upperRightCornereAvatar.click();
+        return this;
+    }
+}
